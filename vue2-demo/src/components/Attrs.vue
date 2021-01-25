f66115d92841bcadfa40d23c65284abd68e63cab
wangdong <11096409@bbktel.com>
2021-01-25 10:54:14 +0800

    init

---
+++ vue2-demo/src/components/Attrs.vue
@@ -0,0 +1,25 @@
+<template>
+  <label>
+    {{ label }}
+    <input
+      id="2"
+      v-bind="$attrs"
+      v-bind:value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    >
+  </label>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false, // 要不要将组件的非prop属性透传到组件根节点上
+  props: ['label', 'value'],
+  created () {
+    console.log(this.$attrs) // 自定义指定父组件的非prop属性透传目标
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2-demo/src/components/Ref.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="item in items" :key="item.message" ref="c">
+    {{ item.message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]}
+  },
+  created () {
+    this.$nextTick(()=> {
+      console.log(this.$refs.c)
+    })
+
+  }
+}
+</script>
+
+<style>
+
+</style>
